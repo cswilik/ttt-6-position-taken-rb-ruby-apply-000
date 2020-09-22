e7ec751eb1ca09a6c4886d432d1b0e7f1a2efe70
Chelsey <cswilik@gmail.com>
2020-09-22 18:21:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board,input)
   if board[input] != " " || board[input] != ""
-    return false 
+    return false
+  elseif board[input] == "X"|| board[iput] == "O"
+    return true
   end 
-end
+end 
+
