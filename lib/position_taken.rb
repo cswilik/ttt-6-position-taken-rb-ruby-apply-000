91ee3e56a3388d077ee506b961ea3a778f8ac9c1
Chelsey <cswilik@gmail.com>
2020-09-22 18:27:39 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -7,10 +7,3 @@ def position_taken?(board, input)
   end 
 end 
 
-# def position_taken?(board, index)
-#   if board[index] == " " || board[index] == "" || board[index] == nil
-#     return false
-#   else
-#     return true
-#   end
-# end
