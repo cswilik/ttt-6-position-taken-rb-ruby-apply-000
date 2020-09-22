fc1900c96568aab30fe30793848f4feac706916e
Chelsey <cswilik@gmail.com>
2020-09-22 18:26:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,16 @@
 # code your #position_taken? method here!
-def position_taken?(board,input)
-  if board[input] != " " || board[input] != ""
+def position_taken?(board, input)
+  if board[input] == " " || board[input] == "" || board[index] == nil
     return false
   else
     return true
   end 
 end 
 
-
+# def position_taken?(board, index)
+#   if board[index] == " " || board[index] == "" || board[index] == nil
+#     return false
+#   else
+#     return true
+#   end
+# end
